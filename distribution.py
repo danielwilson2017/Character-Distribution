53b740d23f7aafe947e4b53bc70e456d8def37c9
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-02-23 14:16:58 -0500

    Updated from Brython Server: 2/23/2016 2:16:59 PM

--- distribution.py
+++ distribution.py
@@ -35,48 +35,14 @@ Notice about this example:
   For example, in the printout above, the letters e, h, l, p and y both occur twice 
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
-"""
-'''
+
 string = str(input("Please enter a string of text (the bigger the better): "))
 b = (int(len(string)))
 print('The distribution of characters in "' + string + '." is :') 
 l = list(string)
 sost = string.split(' ')
 
-a = string1.count("a")
-b = string1.count("b")
-c = string1.count("c")
-d = string1.count("d")
-e = string1.count("e")
-f = string1.count("f")
-g = string1.count("g")
-h = string1.count("h")
-i = string1.count("i")
-j = string1.count("j")
-k = string1.count("k")
-l = string1.count("l")
-m = string1.count("m")
-n = string1.count("n")
-o = string1.count("o")
-p = string1.count("p")
-q = string1.count("q")
-r = string1.count("r")
-s = string1.count("s")
-t = string1.count("t")
-u = string1.count("u")
-v = string1.count("v")
-w = string1.count("w")
-x = string1.count("x")
-y = string1.count("y")
-z = string1.count("z")
-list1 = [] 
-
-list1.append([a,b,c, d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z])
-list1.sort()
-
-
-print(list1)
-''''
+"""
 
 
 
@@ -89,6 +55,7 @@ string1 = string.lower()
 print('The distribution of characters in "' + string + '." is :') 
 ap = "abcdefghijklmnopqrstuvwxyz"
 result=[]
+spil = 
 
 for c in ap:
     r = string.count(c)
@@ -96,3 +63,9 @@ for c in ap:
         t = (r*c)
         result.append(t)
 print(result)
+
+
+
+
+
+
