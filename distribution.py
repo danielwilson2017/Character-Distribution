dbdb211b6ac09adc09a89dfac4435f93a8efe116
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-02-26 14:21:15 -0500

    Updated from Brython Server: 2/26/2016 2:21:16 PM

--- distribution.py
+++ distribution.py
@@ -36,14 +36,6 @@ Notice about this example
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 
-string = str(input("Please enter a string of text (the bigger the better): "))
-b = (int(len(string)))
-print('The distribution of characters in "' + string + '." is :') 
-l = list(string)
-sost = string.split(' ')
-bsort(result, compair)
-print(result)
-
 """
 
 
@@ -76,6 +68,8 @@ print(ln)
 print(list(lists))
 lists.sort(reverse=True)
 print(list(lists))
+if x in range(0:100):
+    
 
 def bsort(seq, cmp):
     """
